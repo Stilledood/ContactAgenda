05a5f526b596b798f8c003861c829ced71785e28
Florin <74854275+Stilledood@users.noreply.github.com>
2022-10-31 20:14:05 +0200

    Added database function fetch all the events from events table

--- database.py
+++ database.py
@@ -7,8 +7,8 @@ from databasedata import username,password
 def create_check_database():
     '''Function to check if the database exists-if it not exists it creates the database and all the tables '''
 
-    mydb=mysql.connector.connect(host= 'localhost', user=username, password= password,consume_results=True)
-    mycursor=mydb.cursor(buffered=True)
+    mydb = mysql.connector.connect(host ='localhost', user =username, password =password, consume_results =True)
+    mycursor = mydb.cursor(buffered = True)
     mycursor.execute('SHOW DATABASES')
 
     if ('personalagenda',) in mycursor:
@@ -18,11 +18,10 @@ def create_check_database():
         mycursor.execute('CREATE DATABASE personalagenda')
         mycursor.close()
 
-        #Creating the contacts table
+        # Creating the contacts table
         mydb = mysql.connector.connect(host='localhost', user=username, password=password, consume_results=True)
-        mycursor = mydb.cursor(buffered=True)
+        mycursor = mydb.cursor(buffered =True)
         mycursor.execute('USE personalagenda')
-
         mycursor.execute("""CREATE TABLE contacts(
                          id INT  AUTO_INCREMENT PRIMARY KEY,
                          first_name VARCHAR(255) DEFAULT 'no name',
@@ -37,9 +36,9 @@ def create_check_database():
         mycursor.close()
 
 
-        #Creating the events table
+        # Creating the events table
         mydb = mysql.connector.connect(host='localhost', user=username, password=password, consume_results=True)
-        mycursor = mydb.cursor(buffered=True)
+        mycursor = mydb.cursor(buffered =True)
         mycursor.execute('USE personalagenda')
         mycursor.execute("""CREATE TABLE events(
                          id INT  AUTO_INCREMENT PRIMARY KEY,
@@ -48,8 +47,7 @@ def create_check_database():
                          date_added TIMESTAMP DEFAULT NOW(),
                          finalizing_date DATETIME)"""
                          )
-        #Commiting all the changes
-
+        # Commiting all the changes
         mycursor.close()
 
 
@@ -58,10 +56,10 @@ def display_all_contacts():
     '''Function to fetch all the contacts from contacts table'''
 
     mydb = mysql.connector.connect(host='localhost', user=username, password=password, database='personalagenda')
-    mycursor=mydb.cursor()
+    mycursor = mydb.cursor()
     query='SELECT * FROM contacts'
     mycursor.execute(query)
-    records=mycursor.fetchall()
+    records = mycursor.fetchall()
     mycursor.close()
     for record in records:
         print(record)
@@ -69,19 +67,42 @@ def display_all_contacts():
 
 
 
-def add_contact(first_name,last_name,email,phone,adress,city,country):
+
+
+def add_contact(first_name, last_name, email,phone, adress, city, country):
     '''Function to add a contact in table contacts'''
 
-    mydb=mysql.connector.connect(host='localhost',user=username,password=password,database='personalagenda')
-    mycursor=mydb.cursor()
+    mydb = mysql.connector.connect(host='localhost', user=username, password=password, database='personalagenda')
+    mycursor = mydb.cursor()
     #Adding the contact
-    sql_query="INSERT INTO contacts(first_name,last_name,email,phone,adress,city,country) VALUES(%s, %s, %s, %s, %s, %s ,%s)"
-    val=(first_name,last_name,email,phone,adress,city,country)
+    sql_query = "INSERT INTO contacts(first_name,last_name,email,phone,adress,city,country) VALUES(%s, %s, %s, %s, %s, %s ,%s)"
+    val = (first_name, last_name, email, phone, adress, city, country)
     print(sql_query)
-    mycursor.execute(sql_query,val)
+    mycursor.execute(sql_query, val)
     mydb.commit()
 
     mycursor.close()
+
+
+
+def fetch_all_events():
+    '''Function to fetch all events from events table'''
+
+    mydb = mysql.connector.connect(host='localhost', user=username, password=password, database='personalagenda')
+    mycursor = mydb.cursor()
+    sql_query = "SELECT * FROM events"
+    try:
+        mycursor.execute(sql_query)
+        records = mycursor.fetchall()
+    except:
+        records = 'No events added'
+    if not records:
+        records = 'No events added'
+    print(records)
+
+
+
+fetch_all_events()
 
 
 
