cb30ef6a8ffd8655ab56ce3185163739e63a4ce2
Florin <74854275+Stilledood@users.noreply.github.com>
2022-11-02 18:40:55 +0200

    Added Treeview to display all contacts from database

--- MainInterface.py
+++ MainInterface.py
@@ -10,7 +10,8 @@ class SampleApp(tk.Tk):
 
     def __init__(self):
         tk.Tk.__init__(self)
-        self.geometry('800x600')
+        self.geometry("800x650+351+174")
+
         self._frame=None
         self.switch_frame(StartPage)
 
@@ -31,9 +32,9 @@ class StartPage(tk.Frame):
 
     def __init__(self,master):
         tk.Frame.__init__(self,master)
-        tk.Label(self,text='Personal Agenda').pack(side='top', fill ='x', pady =10)
-        tk.Button(self,text='Contacts',command= lambda : master.switch_frame(Contacts)).pack()
-        tk.Button(self,text='Event Planner',command = lambda : master.switch_frame(Planner)).pack()
+        tk.Label(self, text='Personal Agenda').pack(side='top', fill ='x', pady =10)
+        tk.Button(self, text='Contacts',command= lambda : master.switch_frame(Contacts)).pack()
+        tk.Button(self, text='Event Planner',command = lambda : master.switch_frame(Planner)).pack()
 
 
 
@@ -44,7 +45,7 @@ class Contacts(tk.Frame):
 
     def __init__(self,master):
         tk.Frame.__init__(self,master)
-        self.master=master
+        self.master = master
 
         # Check if the database exists-if not-create the database and tables
         database.create_check_database()
@@ -65,7 +66,32 @@ class Contacts(tk.Frame):
         self.entry_contact_phone = tk.Entry(self.master).place(rely=0.35, relx=0.6, relheight=0.028, anchor='ne')
 
 
-        
+        #Adding buttons to add/updarte /delete a contact and to clear the form
+
+        self.add_button=tk.Button(self.master, text='Add Contact' ,font=('Helvetica', 8), bg='grey', fg='white').place(rely=0.45,relx=0.4,anchor='ne')
+        self.update_contact=tk.Button(self.master, text= 'Update Contact', font=('Helvetica', 8),bg= 'grey', fg= 'white').place(rely=0.45, relx=0.52, anchor='ne')
+        self.delete_contact = tk.Button(self.master, text='Delete Contact', font=('Helvetica', 8), bg='grey', fg='white').place(rely=0.45, relx=0.64,anchor='ne')
+        self.clear_data= tk.Button(self.master, text='Clear', font=('Helvetica', 8),bg='grey', fg='white').place(rely=0.45, relx=0.70, anchor='ne')
+
+        #Adding a tree to display contacts from database
+        columns = ('First Name', 'Last Name', 'Email', 'Phone')
+        self.contact_display = ttk.Treeview(self.master, show='headings',height='10', columns=columns)
+        self.contact_display.place(rely=0.7,relx=0.5,width=500,anchor='center')
+
+        self.contact_display.heading('First Name', text='First Name', anchor='center')
+        self.contact_display.column('First Name',width=70)
+        self.contact_display.heading('Last Name', text='Last Name', anchor='center')
+        self.contact_display.column('Last Name', width=70)
+        self.contact_display.heading('Email', text='E-mail', anchor='center')
+        self.contact_display.column('Email', width=70)
+        self.contact_display.heading('Phone', text='Phone Number', anchor='center')
+        self.contact_display.column('Phone', width=70)
+
+
+
+
+
+
 
 
 
